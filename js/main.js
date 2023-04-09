a346ba5e30107d05bbd077232c6ac2d3a25bee84
kimagin <info@kimagination.net>
2023-04-09 02:06:27 +0200

    Initialization of the project on github

---
+++ js/main.js
@@ -0,0 +1,48 @@
+'use strict'
+
+//ToolBox
+import {
+  delay, // Asynchronus delay function : delay(time)
+  log, // shorthand console.log : log()
+  select, //Custom querrySelector : select(element, ?all[true:false])
+  event, // Custom event listener : event(element,event,callback)
+  classlist, // Class manipulator : classlist(element,action["+"(add),"-"(remove),"x"(toggle)],class(string or [])),
+  debounce, // Debounce ( runs the function only after the specified delay ) : debounce(function,delay)
+  throttle, // Throttle ( runs the function n times per specified amount time ) : throttle(function,interval)
+  random, // Random number generator : random(min,max)
+} from './utils'
+
+//Imports
+import Alpine from 'alpinejs'
+
+// Setup before DOM loads
+const initApp = async () => {
+  // 🗻 Alpine modules
+  window.Alpine = Alpine
+  Alpine.data('data', Data)
+  Alpine.start()
+}
+
+event(document, 'DOMContentLoaded', initApp)
+
+function Data() {
+  return {
+    init() {},
+    appInfo: {
+      icon: '🌄',
+      title: `<h1><b>AlpineJS</b> + <b>TailwindCSS</b> <br> StarterTemplate</h1>`,
+    },
+    cardInfo: {
+      tag: 'perfume',
+      title: 'Gabrielle Essence Eau De Parfum',
+      description:
+        'A floral, solar and voluptuous interpretation composed by Olivier Polge, Perfumer-Creator for the House of CHANEL.',
+      price: 149.99,
+      exPrice: 169.99,
+      isInCart: false,
+      quantity: 0,
+    },
+  }
+}
+
+// 🚩 JS Code comes here
---
+++ tailwind.config.cjs
@@ -0,0 +1,50 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+    //Define Media queries
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+
+    //Define Custom Fonts
+    fontFamily: {
+      serif: ['Fraunces', 'serif'], //or alternatively
+      sans: ['Montserrat', 'sans-serif'], //and then you can apply it by 'font-just'
+    },
+    extend: {
+      //Extend Tailwind Color Pallet
+      colors: {
+        //Generated with Uicolors.app
+        'dark-cyan': 'hsl(158, 36%, 37%)',
+        cream: 'hsl(30, 38%, 92%)',
+        'very-dark-blue': 'hsl(212, 21%, 14%),',
+        'dark-grayish-blue': 'hsl(228, 12%, 48%)',
+        white: 'hsl(0, 0%, 100%)',
+      },
+      //Create Custom Animations
+      animation: {
+        'spin-slow': 'spin 3s linear infinite',
+        //You can call the keyframes
+        bounce: 'bounce 2s ease-in-out infinite',
+      },
+
+      //Create custom Key-frames
+      keyframes: {
+        bounce: {
+          //You can use css .style in js
+          '0%, 100%': {
+            transform: 'scale(1.1)',
+          },
+          '50%': {
+            transform: 'scale(.9)',
+          },
+        },
+      },
+    },
+  },
+  plugins: [],
+}
